6142faba9e1d3978b90f76f713dd7a4a3ede05f4
aguuung <aguuung19@gmail.com>
2023-05-29 16:45:49 +0800

    landing page setting index progress

--- resources/views/admin/Landing-Page/index.blade.php
+++ resources/views/admin/Landing-Page/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.home')
 
 @section('content')
-test
+    <section class="landing-page-setting" data-aos="zoom-in" data-aos-delay="100">
+       <div class="row">
+            <div class="col">
+                <h1 class="lp-header">Landing Page Setting</h1> 
+            </div>
+            <div class="col align-self-center text-center text-lg-end pt-3 pt-lg-0 pt-md-0">
+                <a href="#" class="btn btn-lp-edit"><i class="bi bi-pencil-square"></i> Edit</a>
+            </div>
+       </div>
+       <div class="form-group pt-5">
+        <h2 class="lp-subheader">Logo</h2>
+       </div>
+
+    </section>
 @endsection
